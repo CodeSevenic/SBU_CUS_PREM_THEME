d7398d786e3527305908421b97cc10ed23b9d1e1
CodeSevenic <sbu.audio.production@gmail.com>
2021-09-24 09:28:53 +0200

    added gulp clean functionalityy

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,58 @@
-import yargs from 'yargs';
 import gulp from 'gulp';
-const sass = require('gulp-sass')(require('sass'));
 import cleanCSS from 'gulp-clean-css';
 import gulpIf from 'gulp-if';
 import sourceMaps from 'gulp-sourcemaps';
+import imagemin from 'gulp-imagemin';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import del from 'del';
 
-const PRODUCTION = yargs.argv.prod;
+const sass = gulpSass(dartSass);
+
+let PRODUCTION = false;
+
+const paths = {
+  styles: {
+    src: ['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'],
+    dest: 'dist/assets/css',
+  },
+  images: {
+    src: 'src/assets/images/**/*.{jpg,jpeg,png,svg,gif}',
+    dest: 'dist/assets/images',
+  },
+  other: {
+    src: [
+      'src/assets/**/*',
+      '!src/assets/{images,js,scss}',
+      '!src/assets/{images,js,scss}/**/*',
+    ],
+    dest: 'dist/assets',
+  },
+};
+
+export const clean = () => del(['dist']);
 
 export const styles = () => {
   return gulp
-    .src(['src/assets/scss/bundle.scss', 'src/assets/scss/admin.scss'])
+    .src(paths.styles.src)
     .pipe(gulpIf(!PRODUCTION, sourceMaps.init()))
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulpIf(PRODUCTION, cleanCSS({ compatibility: 'ie8' })))
     .pipe(gulpIf(!PRODUCTION, sourceMaps.write()))
-    .pipe(gulp.dest('dist/assets/css'));
+    .pipe(gulp.dest(paths.styles.dest));
+};
+
+export const images = () => {
+  return gulp
+    .src(paths.images.src)
+    .pipe(gulpIf(!PRODUCTION, imagemin()))
+    .pipe(gulp.dest(paths.images.dest));
+};
+
+export const watch = () => {
+  gulp.watch('src/assets/scss/**/*.scss', styles);
+};
+
+export const copy = () => {
+  return gulp.src(paths.other.src).pipe(gulp.dest(paths.other.dest));
 };
--- lib/enqueue-assets.php
+++ lib/enqueue-assets.php
@@ -6,3 +6,10 @@ function sbu_theme_assets()
 }
 
 add_action('wp_enqueue_scripts', 'sbu_theme_assets');
+
+function sbu_theme_admin_assets()
+{
+  wp_enqueue_style('sbu_theme-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all');
+}
+
+add_action('admin_enqueue_scripts', 'sbu_theme_admin_assets');
