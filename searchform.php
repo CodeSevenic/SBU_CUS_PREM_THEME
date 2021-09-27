574bb149e034739e154b00782c7c5245844a4ec3
CodeSevenic <sbu.audio.production@gmail.com>
2021-09-27 20:24:08 +0200

    code update

--- searchform.php
+++ searchform.php
@@ -1,1 +1,12 @@
-JJHGJHGJGGJG
+<form class="c-search-form" role="search" method="GET" action="<?php echo esc_url(home_url('/')) ?>">
+  <label class="c-search-form__label">
+    <span class="screen-reader-text">
+      <?php echo esc_html_x('Search for:', 'label', '_themename') ?>
+    </span>
+  </label>
+  <input class="c-search-form__field" type="search" name="s" placeholder="Search"
+    value="<?php echo esc_attr(get_search_query()) ?>">
+  <button class="c-search-form__button" type="submit"><span
+      class="u-screen-reader-text"><?php echo esc_html_x('Search', 'submit button', '_themename') ?></span><i
+      class="fas fa-search" aria-hidden="true"></i></button>
+</form>
