b8690c877f592448ddf113fbfcb54f2655f6372d
CodeSevenic <sbu.audio.production@gmail.com>
2021-09-28 15:41:53 +0200

    code update

--- src/assets/js/components/navigation.js
+++ src/assets/js/components/navigation.js
@@ -2,6 +2,9 @@ import $ from 'jquery';
 
 $('.c-navigation')
   .on('mouseenter', '.menu-item-has-children', (e) => {
+    if (!$(e.currentTarget).parents('.sub-menu').length) {
+      $('.menu > .menu-item.open').find('> a > .menu-button').trigger('click');
+    }
     $(e.currentTarget).addClass('open');
   })
   .on('mouseleave', '.menu-item-has-children', (e) => {
@@ -10,19 +13,27 @@ $('.c-navigation')
 
 $('.c-navigation').on('click', '.menu .menu-button', (e) => {
   e.preventDefault();
+  e.stopPropagation();
   let menu_button = $(e.currentTarget);
   let menu_link = menu_button.parent();
   let menu_item = menu_link.parent();
 
   if (menu_item.hasClass('open')) {
-    menu_item.removeClass('open');
-    menu_link.attr('aria-expanded', 'false');
+    menu_item.add(menu_item.find('.menu-item.open')).removeClass('open');
+    menu_link.add(menu_item.find('a')).attr('aria-expanded', 'false');
     menu_button.find('.menu-button-show').attr('aria-hidden', 'false');
     menu_button.find('.menu-button-hide').attr('aria-hidden', 'true');
   } else {
+    menu_item.siblings('.open').find('>a>.menu-button').trigger('click');
     menu_item.addClass('open');
     menu_link.attr('aria-expanded', 'true');
     menu_button.find('.menu-button-show').attr('aria-hidden', 'true');
     menu_button.find('.menu-button-hide').attr('aria-hidden', 'false');
+  }
+});
+
+$(document).on('click', (e) => {
+  if ($('.menu-item.open').length) {
+    $('.menu > .menu-item.open > a > .menu-button').trigger('click');
   }
 });
