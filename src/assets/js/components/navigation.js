2a6e5e76efa1f5146961034c85310bbf981becbf
CodeSevenic <sbu.audio.production@gmail.com>
2021-09-28 12:02:30 +0200

    code update

--- src/assets/js/components/navigation.js
+++ src/assets/js/components/navigation.js
@@ -12,12 +12,17 @@ $('.c-navigation').on('click', '.menu .menu-button', (e) => {
   e.preventDefault();
   let menu_button = $(e.currentTarget);
   let menu_link = menu_button.parent();
-  let menu_item = menu_link();
+  let menu_item = menu_link.parent();
+
   if (menu_item.hasClass('open')) {
+    menu_item.removeClass('open');
+    menu_link.attr('aria-expanded', 'false');
+    menu_button.find('.menu-button-show').attr('aria-hidden', 'false');
+    menu_button.find('.menu-button-hide').attr('aria-hidden', 'true');
   } else {
     menu_item.addClass('open');
-    menu_link.attr('aria-button-show', 'true');
+    menu_link.attr('aria-expanded', 'true');
     menu_button.find('.menu-button-show').attr('aria-hidden', 'true');
-    menu_button.find('.menu-button-hide').attr('aria-hidden', 'true');
+    menu_button.find('.menu-button-hide').attr('aria-hidden', 'false');
   }
 });
