3adb901e0218ce50216f4ccff03d80e90b60aa1a
CodeSevenic <sbu.audio.production@gmail.com>
2021-09-30 18:33:50 +0200

    code update

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -6,7 +6,13 @@ $site_info = get_theme_mod('_themename_site_info', '');
   <div class="o-container">
     <div class="o-row">
       <div class="o-row__column o-row__column--span-12 c-site-info__text">
-        <?php echo esc_html($site_info); ?>
+        <?php
+          $allowed = array('a' => array(
+            'href' => array(),
+            'title' => array()
+          ));
+
+          echo wp_kses($site_info, $allowed); ?>
       </div>
     </div>
   </div>
