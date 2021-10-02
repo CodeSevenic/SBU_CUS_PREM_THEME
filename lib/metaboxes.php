e8779ed4658f6df8bd418b200d495175c72a85b4
CodeSevenic <sbu.audio.production@gmail.com>
2021-10-02 13:11:55 +0200

    code update

--- lib/metaboxes.php
+++ lib/metaboxes.php
@@ -9,7 +9,8 @@ add_action('add_meta_boxes', '_themename_add_meta_box');
 
 function _themename_post_metabox_html($post)
 {
-  $subtitle = get_post_meta($post->ID, '__themename_post_subtitle', true)
+  $subtitle = get_post_meta($post->ID, '__themename_post_subtitle', true);
+  wp_nonce_field('_themename_update_post_metabox', '_themename_update_post_nonce');
 ?>
 <p>
   <label for="_themename_post_metabox_html"><?php esc_html_e('Post Subtitle', '_themename'); ?></label>
@@ -17,12 +18,28 @@ function _themename_post_metabox_html($post)
   <input class="widefat" type="text" name="_themename_post_subtitle_field" id="_themename_post_metabox_html"
     value="<?php echo esc_attr($subtitle); ?>">
 </p>
+<p>
+  <label for="_themename_post_layout_field"><?php esc_html_e('Layout', '_themename') ?></label>
+  <select name="_themename_post_layout_field" id="_themename_post_layout_field" class="widefat">
+    <option value="full"><?php esc_html_e('Full Width', '_themename') ?></option>
+    <option value="sidebar"><?php esc_html_e('Post With Sidebar', '_themename') ?></option>
+  </select>
+</p>
 <?php } ?>
 
 <?php
 
 function _themename_save_post_metabox($post_id, $post)
 {
+  $edit_cap = get_post_type_object($post->post_type)->cap->edit_post;
+  if (!current_user_can($edit_cap, $post_id)) {
+    return;
+  }
+
+  if (!isset($_POST['_themename_update_post_nonce']) || !wp_verify_nonce($_POST['_themename_update_post_nonce'], '_themename_update_post_metabox')) {
+    return;
+  }
+
   if (array_key_exists('_themename_post_subtitle_field', $_POST)) {
     update_post_meta($post_id, '__themename_post_subtitle', sanitize_text_field($_POST['_themename_post_subtitle_field']));
   }
