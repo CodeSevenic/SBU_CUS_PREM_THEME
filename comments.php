a1e2661743c97403d620559a98cc75e092087c8d
CodeSevenic <sbu.audio.production@gmail.com>
2021-10-08 15:32:01 +0200

    code update

--- comments.php
+++ comments.php
@@ -1,48 +1,48 @@
 <?php
 
 if (post_password_required()) {
-  return;
-} ?>
+    return;
+}?>
 
 <div id="comments" class="c-comments">
 
-  <?php if (have_comments()) { ?>
+  <?php if (have_comments()) {?>
     <h2 class="c-comments__title">
       <?php
-      /* translators: 1 is number of comments and 2 is post title */
-      printf(
+/* translators: 1 is number of comments and 2 is post title */
+    printf(
         esc_html(_n(
-          '%1$s Reply to "%2$s"',
-          '%1$s Replies to "%2$s"',
-          get_comments_number(),
-          '_themename'
+            '%1$s Reply to "%2$s"',
+            '%1$s Replies to "%2$s"',
+            get_comments_number(),
+            '_themename'
         )),
         number_format_i18n(get_comments_number()),
         get_the_title()
-      );
-      ?>
+    );
+    ?>
     </h2>
 
     <ul class="c-comments__list">
       <?php
-      wp_list_comments(array(
+wp_list_comments(array(
         'short_ping' => false,
         'avatar_size' => 50,
         'reply_text' => 'Hello',
-        'callback' => '_themename_comment_callback'
-      ));
-      ?>
+        'callback' => '_themename_comment_callback',
+    ));
+    ?>
     </ul>
 
-    <?php the_comments_pagination(); ?>
-  <?php } ?>
+    <?php the_comments_pagination();?>
+  <?php }?>
 
   <?php
-  if (!comments_open() && get_comments_number()) { ?>
-    <p class="c-comments__closed"><?php esc_html_e('Comments are closed for this post', '_themename'); ?></p>
-  <?php } else { ?>
+if (!comments_open() && get_comments_number()) {?>
+    <p class="c-comments__closed"><?php esc_html_e('Comments are closed for this post', '_themename');?></p>
+  <?php } else {?>
 
-    <?php comment_form(); ?>
+    <?php comment_form();?>
 
-  <?php } ?>
+  <?php }?>
 </div>
