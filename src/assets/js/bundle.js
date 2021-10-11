015822253de51dbaa7fd2cc802a68c9885bbe81a
CodeSevenic <sbu.audio.production@gmail.com>
2021-10-11 20:32:28 +0200

    code update

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -1,3 +1,11 @@
 import $ from 'jquery';
 import './components/slider';
 import './components/navigation';
+import 'slick-carousel';
+
+$(() => {
+  $('.c-post__gallery, .blocks-gallery-grid').slick({
+    arrows: false,
+    adaptiveHeight: true,
+  });
+});
