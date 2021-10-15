29a4d84dcdcff0507f2cea415781f96adfb9326c
CodeSevenic <sbu.audio.production@gmail.com>
2021-10-15 16:11:52 +0200

    code update

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -3,9 +3,9 @@ import './components/slider';
 import './components/navigation';
 import 'slick-carousel';
 
-$(() => {
-  $('.c-post__gallery, .blocks-gallery-grid').slick({
-    arrows: false,
-    adaptiveHeight: true,
-  });
-});
+// $(() => {
+//   $('.c-post__gallery, .blocks-gallery-grid').slick({
+//     arrows: false,
+//     adaptiveHeight: true,
+//   });
+// });
