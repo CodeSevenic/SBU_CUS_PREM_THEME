ca5afc843ad29eaf5ef0046ec313b758b9ebcad0
CodeSevenic <sbu.audio.production@gmail.com>
2021-10-16 18:24:19 +0200

    code update

--- src/assets/js/bundle.js
+++ src/assets/js/bundle.js
@@ -10,16 +10,19 @@ $(() => {
   // });
   $('.most_recent_widget').slick();
 
-  wp.customize.selectiveRefresh.bind(
-    'partial-content-rendered',
-    (placement) => {
-      console.log(placement);
-      if (
-        placement.partial.widgetParts &&
-        placement.partial.widgetParts.idBase === '_themename_mst_recent_widget'
-      ) {
-        placement.container.find('.most_recent_widget').slick();
+  if (wp.customize && wp.customize.selectiveRefresh) {
+    wp.customize.selectiveRefresh.bind(
+      'partial-content-rendered',
+      (placement) => {
+        console.log(placement);
+        if (
+          placement.partial.widgetIdParts &&
+          placement.partial.widgetIdParts.idBase ===
+            '_themename_mst_recent_widget'
+        ) {
+          placement.container.find('.most_recent_widget').slick();
+        }
       }
-    }
-  );
+    );
+  }
 });
